343e1a41f8630662f4a08a87bf83a334d81053df
Eugen Mueller <eugen.mllr@gmail.com>
2018-03-20 21:22:18 +0100

    Removed unneccessary comments.

--- app/services/import_navision_items.rb
+++ app/services/import_navision_items.rb
@@ -6,32 +6,4 @@ class ImportNavisionItems < BaseImporter
   def call
     import_all
   end
-
-  # private
-
-  # def import_items
-  #   Spree::NavisionItem.transaction do
-  #     # default batch_size 1000
-  #     NavConnector::Item.find_in_batches do |group|
-  #       group.each { |raw_item| create_or_update_item(raw_item) }
-  #     end
-  #   end
-  # rescue StandardError => exception
-  #   OpenStruct.new(success?: false, error: exception)
-  # else
-  #   OpenStruct.new(success?: true, error: nil)
-  # end
-
-  # def create_or_update_item(item)
-  #   sliced_attributes = item.slice(*model_attributes)
-
-  #   Spree::NavisionItem.update_or_create_by(
-  #     { no: sliced_attributes[:no] },
-  #     sliced_attributes
-  #   )
-  # end
-
-  # def model_attributes
-  #   @model_attributes ||= Spree::NavisionItem.attribute_names.map(&:to_sym)
-  # end
 end
